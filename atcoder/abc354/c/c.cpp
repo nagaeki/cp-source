67d9f6b415b2c58eab07028c3fa30702bd6ac9ba
eki <i@eki.moe>
2024-05-20 10:20:00 +0900

    finish abc354 c

--- atcoder/abc354/c/c.cpp
+++ atcoder/abc354/c/c.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 struct card {
@@ -8,14 +9,31 @@ struct card {
 	int C;
 };
 
-int N;
+int N, v;
 vector<card> cards;
+vector<int> ans;
 
 int main() {
 	cin >> N;
-	for (int i = 1;i <= n;i++) {
+	for (int i = 1;i <= N;i++) {
 		int ai, ci;
 		cin >> ai >> ci;
-		cards.
+		card pushCard = card{ i,ai,ci };
+		cards.push_back(pushCard);
 	}
+	sort(cards.begin(), cards.end(), [](card a, card b) {
+		return a.C < b.C;
+		});
+	for (int i = 0;i < N;i++) {
+		if (cards.at(i).A > v) {
+			v = cards.at(i).A;
+			ans.push_back(cards.at(i).id);
+		}
+	}
+	sort(ans.begin(), ans.end());
+	cout << ans.size() << endl;
+	for (int i = 0;i < ans.size();i++) {
+		cout << ans.at(i) << " ";
+	}
+	return 0;
 }
